2dd6e77e39dbf4e32c8823f339493c6291f080b7
santluz <esantluz@gmail.com>
2026-03-13 23:17:14 -0300

    fix dashboard layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,5 +1,11 @@
 'use client';
-import { AppLayout } from '@/components/layout/AppLayout';
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return <AppLayout>{children}</AppLayout>;
+import { AppLayout } from '@/components/layout/AppLayout'
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return <AppLayout>{children}</AppLayout>
 }
+
